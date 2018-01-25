938f4dea1758b4246c6b2d59ba7b9bc14502d1de
Ergo117 <peter.irie.wang@gmail.com>
2018-01-24 17:21:30 -0800

    base files

---
+++ server/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+// UNCOMMENT THE DATABASE YOU'D LIKE TO USE
+// var items = require('../database');
+
+
+var app = express();
+
+// UNCOMMENT FOR ANGULAR
+// app.use(express.static(__dirname + '/../client'));
+// app.use(express.static(__dirname + '/../node_modules'));
+
+app.get('/items', function (req, res) {
+  items.selectAll(function(err, data) {
+    if(err) {
+      res.sendStatus(500);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log('listening on port 3000!');
+});
+
