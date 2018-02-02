1c67ffb0889e5ac9d76d136aee816d4124443b98
Ergo117 <peter.irie.wang@gmail.com>
2018-02-01 20:03:50 -0800

    database restructure and auth

--- server/server.js
+++ server/server.js
@@ -4,20 +4,32 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database');
 const toneAnalyzer = require('./helpers/toneAnalyzer');
-
 const app = express();
+const sequelize = require('../database/index.js').sequelize;
+const User = require('../database/models/User.js');
+const passport = require('passport');
+const social = require('./passport/authRoute.js')(app, passport);
 
 
-app.use(express.static(path.join(__dirname, '/../client')));
-app.use(express.static(path.join(__dirname, '/../node_modules')));
+// app.use((req, res, next)=>{
+//   if (req.user) {  
+//   debugger;
+//   }
+//   next();
+// });
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/../client')));
+app.use(express.static(path.join(__dirname, '/../node_modules')));
 
 
 app.get('/api/users', (req, res) => {
   res.sendStatus(200);
 });
+
 
 app.post('/ibmtone', (req, res) => {
  var test = 'I am very new to Node and am still getting used to its conventions. Thanks for the help!';
