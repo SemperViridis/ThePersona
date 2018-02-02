8ebe600f463b46678b420e51c6f73129bae9089f
Ergo117 <peter.irie.wang@gmail.com>
2018-02-01 20:32:35 -0800

    env for facebook auth

--- server/passport/authRoute.js
+++ server/passport/authRoute.js
@@ -44,8 +44,8 @@ module.exports = function (app, passport) {
   // Facebook Strategy
 
   passport.use(new FacebookStrategy({
-    clientID: FACEBOOK.CLIENT_ID,
-    clientSecret: FACEBOOK.CLIENT_SECRET,
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'photos', 'email']
   },
