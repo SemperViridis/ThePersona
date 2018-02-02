d8cc286dc970ddaad87adafd95db4d9ba80d5789
David Inoa <davidinoa@gmail.com>
2018-02-01 21:51:11 -0500

    Enable stream to render

--- client/components/recorder.js
+++ client/components/recorder.js
@@ -1,20 +1,34 @@
 angular.module('app')
   .controller('recorderController', function () {
+    this.recordedBlobs = [];
     this.mediaSource = new MediaSource();
     this.mediaSource.addEventListener('sourceopen', this.handleSourceOpen, false);
 
-    this.recorderVideo = angular.element(document).find('recorder');
-    this.recordButton = angular.element(document).find('record');
+    this.recorderVideo = document.querySelector('video#recorder');
+    this.recordButton = document.querySelector('button#record');
 
-    this.constraints = {
-      audio: true,
-      video: true
-    };
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+      .then(stream => this.handleSuccess(stream));
 
     this.handleSourceOpen = () => {
-      console.log('MediaSource opened');
       this.sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
-      console.log('Source buffer: ', sourceBuffer);
+    };
+
+    this.handleDataAvailable = (event) => {
+      if (event.data && event.data.size > 0) {
+        this.recordedBlobs.push(event.data);
+      }
+    };
+
+    this.handleSuccess = (stream) => {
+      this.stream = stream;
+      this.recorderVideo.srcObject = stream;
+    };
+
+    this.startRecording = () => {
+      this.mediaRecorder = new MediaRecorder(this.stream, { mimeType: 'video/webm;codecs=vp9' });
+      this.mediaRecorder.ondataavailable = this.handleDataAvailable;
+      this.mediaRecorder.start(10);
     };
   })
 
