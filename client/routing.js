08ba1fc216ed6b19b4d983ec51cdf948e9b08afd
Patty Kovash <pattykovash@gmail.com>
2018-02-03 13:08:56 -0700

    Refactor routing.js to create state via looping construct

--- client/routing.js
+++ client/routing.js
@@ -1,47 +1,48 @@
 angular.module('app', ['ui.router'])
   .config(function ($stateProvider, $urlRouterProvider) {
-    const appState = {
-      name: 'app',
-      url: '/',
-      component: 'app'
-    };
-
-    const logInState = {
-      name: 'login',
-      url: '/login',
-      component: 'login'
-    };
-
-    const interviewState = {
-      name: 'app.interview',
-      url: 'interview',
-      component: 'interview'
-    };
-
-    const practiceState = {
-      name: 'app.interview.practice',
-      url: '/practice',
-      component: 'practice'
-    };
-
-    const mockState = {
-      name: 'app.interview.mock',
-      url: '/mock',
-      component: 'mock'
-    };
-
-    const communityState = {
-      name: 'app.community',
-      url: 'community',
-      component: 'community'
-    };
+    const states = [
+      {
+        name: 'app',
+        url: '/',
+        component: 'app'
+      },
+      {
+        name: 'app.interview',
+        url: 'interview',
+        component: 'interview'
+      },
+      {
+        name: 'app.interview.practice',
+        url: '/practice',
+        component: 'practice'
+      },
+      {
+        name: 'app.interview.mock',
+        url: '/mock',
+        component: 'mock'
+      },
+      {
+        name: 'app.community',
+        url: 'community',
+        component: 'community'
+      },
+      {
+        name: 'app.user',
+        url: '/{ id }',
+        component: 'user'
+        // NEED TO ADD RESOLVE SERVICE TO RETRIEVE USER ID
+      },
+      {
+        name: 'login',
+        url: '/login',
+        component: 'login'
+      }
+    ];
 
     $urlRouterProvider.otherwise('/');
-    $stateProvider.state(appState);
-    $stateProvider.state(logInState);
-    $stateProvider.state(interviewState);
-    $stateProvider.state(practiceState);
-    $stateProvider.state(mockState);
-    $stateProvider.state(communityState);
+
+    states.forEach((state) => {
+      $stateProvider.state(state);
+    });
   });
 
