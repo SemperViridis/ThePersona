886ad13b6f30e930d22e305adba6944b9ac1cef7
David Inoa <davidinoa@gmail.com>
2018-02-05 17:59:08 -0500

    Allow user to download their interview recording

--- client/components/playback.js
+++ client/components/playback.js
@@ -27,6 +27,21 @@ angular.module('app')
         }
       });
     };
+
+    this.download = () => {
+      const blob = new Blob(this.recordedBlobs, { type: 'video/webm' });
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.style.display = 'none';
+      a.href = url;
+      a.download = 'recording.webm';
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+      }, 100);
+    };
   })
   .component('playback', {
     controller: 'playbackController',
