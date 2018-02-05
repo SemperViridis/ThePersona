5bd257478bb8bfbff8de15a2267c888b36b53052
David Inoa <davidinoa@gmail.com>
2018-02-05 14:05:46 -0500

    Add method to allow playing recorded video

--- client/components/playback.js
+++ client/components/playback.js
@@ -1,4 +1,30 @@
 angular.module('app')
+  .controller('playbackController', function () {
+    // properties
+    this.recorderBlobs = []; // need to retrieve this from recorder
+    this.recorderVideo = document.querySelector('video#recorded');
+    this.playButton = document.querySelector('button#play');
+    this.downloadButton = document.querySelector('button#download');
+
+    // methods
+    this.play = () => {
+      const { recorderVideo } = this;
+      const superBuffer = new Blob(this.recorderBlobs);
+      recorderVideo.src = window.URL.createObjectURL(superBuffer);
+      recorderVideo.addEventListener('loadedmetadata', () => {
+        if (recorderVideo.duration === Infinity) {
+          recorderVideo.currentTime = 1e101;
+          recorderVideo.ontimeupdate = () => {
+            recordedVideo.currentTime = 0;
+            recorderVideo.ontimeupdate = () => {
+              delete recorderVideo.ontimeupdate;
+              recorderVideo.play();
+            };
+          };
+        }
+      });
+    };
+  })
   .component('playback', {
     templateUrl: 'templates/playback.html'
   });
