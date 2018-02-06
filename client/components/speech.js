1fd8a59f9027f134ca3a2cb36a50901235b290f2
David Inoa <davidinoa@gmail.com>
2018-02-06 17:56:15 -0500

    Cleanup speech component

--- client/components/speech.js
+++ client/components/speech.js
@@ -7,6 +7,49 @@ angular.module('app')
     this.analysis = '';
     this.submitButton = $('button.large.ui.right.floated.button.submit');
 
+    // initialize speech interface
+    this.recognition = new webkitSpeechRecognition();
+    // set attributes
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+
+    // set event handlers
+    this.recognition.onstart = () => {
+      this.recognizing = true;
+      $scope.$apply();
+    };
+
+    this.recognition.onerror = () => {
+      this.ignoreOnEnd = true;
+    };
+
+    this.recognition.onend = () => {
+      this.recognizing = false;
+      this.responses.push(this.finalTranscript);
+      // final_span.innerHTML = '';
+      this.submitButton.removeAttr('disabled');
+      $scope.$apply();
+      // if (this.ignoreOnEnd) {
+      //   return;
+      // }
+      // if (!this.finalTranscript) {
+      //   return;
+      // }
+    };
+
+    this.recognition.onresult = (event) => {
+      let interimTranscript = '';
+      for (let i = event.resultIndex; i < event.results.length; i += 1) {
+        if (event.results[i].isFinal) {
+          this.finalTranscript += event.results[i][0].transcript + '.';
+        } else {
+          interimTranscript += event.results[i][0].transcript;
+        }
+      }
+      interim_span.innerHTML = interimTranscript;
+      final_span.innerHTML = this.finalTranscript;
+    };
+
     this.handleSubmission = () => {
       this.select();
       console.log('response', this.responses);
@@ -19,61 +62,6 @@ angular.module('app')
         this.fillers(results);
       });
     };
-
-    if ('webkitSpeechRecognition' in window) {
-      // initialize speech interface
-
-      this.recognition = new webkitSpeechRecognition();
-      // set attributes
-      this.recognition.continuous = true;
-      this.recognition.interimResults = true;
-
-      // set event handlers
-      this.recognition.onstart = () => {
-        this.recognizing = true;
-        $scope.$apply();
-      };
-      this.recognition.audiostart = () => {
-        console.log('sound started!');
-      };
-      this.recognition.audioend = () => {
-        console.log('sound ended!');
-      };
-
-      this.recognition.onerror = () => {
-        this.ignoreOnEnd = true;
-      };
-
-      this.recognition.onend = () => {
-        this.recognizing = false;
-        this.responses.push(this.finalTranscript);
-        // final_span.innerHTML = '';
-        this.submitButton.removeAttr('disabled');
-        $scope.$apply();
-        if (this.ignoreOnEnd) {
-          return;
-        }
-        if (!this.finalTranscript) {
-          return;
-        }
-      };
-
-      this.recognition.onresult = (event) => {
-        let interimTranscript = '';
-        for (let i = event.resultIndex; i < event.results.length; i += 1) {
-          if (event.results[i].isFinal) {
-            this.finalTranscript += event.results[i][0].transcript + '.';
-          } else {
-            interimTranscript += event.results[i][0].transcript;
-          }
-        }
-        interim_span.innerHTML = interimTranscript;
-        final_span.innerHTML = this.finalTranscript;
-      };
-    } else {
-      // upgrade
-      console.log('need upgrade');
-    }
 
     this.getNextPrompt = () => {
       this.recognition.stop();
@@ -99,7 +87,6 @@ angular.module('app')
         this.ignoreOnend = false;
         final_span.innerHTML = '';
         interim_span.innerHTML = '';
-        this.startTimestamp = Date.now();
         this.recognition.start();
       } else {
         this.recognition.stop();
