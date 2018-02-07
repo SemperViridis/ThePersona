9be93f735635824b884665b90846522ea381c154
Travis Tillman <travtillman@gmail.com>
2018-02-07 13:57:29 -0500

    (feat) render spider charts on the the results table

--- client/components/radar.js
+++ client/components/radar.js
@@ -2,7 +2,27 @@ angular.module('app').controller('RadarCtrl', function ($scope) {
   $scope.labels = ['Anger', 'Fear', 'Joy', 'Sadness', 'Analytical', 'Confident', 'Tentative'];
 
   $scope.data = [
-    [65, 59, 90, 81, 56, 55, 40],
-    [28, 48, 40, 19, 96, 27, 100]
+    [75, 81, 98, 78, 68, 21, 89]
   ];
-});
+
+console.log('radar this:', this);
+console.log('radar scope:', $scope.$ctrl);
+// console.log('new tone:', this.tone);
+//
+// for (let i = 0; i < this.labels.length; i += 1) {
+//   for (let k = 0; k < this.output.length; k += 1) {
+//     if (this.output[k].tone_name === this.labels[i]) {
+//       this.data[i] = this.output[k].score;
+//     }
+//   }
+// }
+
+})
+
+  .component('radar', {
+    bindings: {
+      output: '<',
+    },
+    controller: 'RadarCtrl',
+    templateUrl: 'templates/radar.html'
+  });
