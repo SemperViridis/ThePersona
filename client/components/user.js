2234c72e7b7c087ac2fdb2d20faedf443a23e042
Patty Kovash <pattykovash@gmail.com>
2018-02-11 18:07:08 -0700

    Add stats styling

--- client/components/user.js
+++ client/components/user.js
@@ -1,9 +1,29 @@
 angular.module('app')
-  .controller('userCtrl', function () {
+  .controller('userCtrl', function ($location) {
     this.username = 'Patty Kovash';
     this.numInterviews = 10;
 
+    this.removeActiveSub = (e) => {
+      const activeElem = document.getElementsByClassName('activeSub')[0];
+      if (activeElem) {
+        activeElem.classList.remove('activeSub');
+      }
+      e.target.classList.add('activeSub');
+    };
 
+    this.userRemoveActive = setInterval(() => {
+      if (document.readyState === 'complete') {
+        clearInterval(this.userRemoveActive);
+        const userActive = document.getElementsByClassName('ui vertical sticky menu dashboardMenu')[0].getElementsByClassName('active')[0];
+        if (userActive) {
+          userActive.classList.remove('active');
+        }
+      }
+    }, 100);
+
+    this.userInit = () => {
+      this.userRemoveActive;
+    };
   })
   .component('user', {
     controller: 'userCtrl',
