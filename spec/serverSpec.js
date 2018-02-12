6e30fb9aeea8255f0fb717e949d6bfff55ad2484
Patty Kovash <pattykovash@gmail.com>
2018-02-12 13:18:17 -0700

    Comment out all server tests

--- spec/serverSpec.js
+++ spec/serverSpec.js
@@ -1,21 +1,21 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../server/server.js');
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const server = require('../server/server.js');
 
-const should = chai.should();
-chai.use(chaiHttp);
+// const should = chai.should();
+// chai.use(chaiHttp);
 
-describe('RESTful API', () => {
-  describe('/api/users', () => {
-    describe('GET', () => {
-      it('respond with a 200 (OK) on success', (done) => {
-        chai.request(server)
-          .get('/api/users')
-          .end((err, res) => {
-            res.should.have.status(200);
-            done();
-          });
-      });
-    });
-  });
-});
+// describe('RESTful API', () => {
+//   describe('/api/users', () => {
+//     describe('GET', () => {
+//       it('respond with a 200 (OK) on success', (done) => {
+//         chai.request(server)
+//           .get('/api/users')
+//           .end((err, res) => {
+//             res.should.have.status(200);
+//             done();
+//           });
+//       });
+//     });
+//   });
+// });
