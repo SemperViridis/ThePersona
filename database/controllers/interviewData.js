a6de3dee0c2f052a74ab69fc36a30eaa2127bfbd
Patty Kovash <pattykovash@gmail.com>
2018-02-14 15:48:12 -0700

    Fix addInterview

--- database/controllers/interviewData.js
+++ database/controllers/interviewData.js
@@ -1,15 +1,27 @@
 const db = require('../../database');
 
-exports.addInterview = (interviewObj) => {
+// Function that creates an Interview instance
+exports.addInterview = (email, interviewObj, callback) => {
   db.User.find({ where: { email: email } })
     .then((user) => {
-      return db.Interview.create({
-        response: interviewObj.response,
-        videoUrl: interviewObj.videoUrl,
-        userId: user.id
-      });
+      if (user) {
+        db.Interview.create({
+          response: interviewObj.response,
+          videoUrl: interviewObj.videoUrl,
+          userId: user.id
+        })
+          .then((interview) => {
+            callback(null, interview);
+          })
+          .catch((err) => {
+            callback(err, null);
+          });
+      }
+      const error = 'No user found';
+      callback(error, null);
     })
     .catch((err) => {
       console.error('ERROR IN DB: ', err);
+      callback(err, null);
     });
 };
