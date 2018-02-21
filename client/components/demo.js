540b61c4f9c8cb2df528c1511bd7556f867153a2
David Inoa <davidinoa@gmail.com>
2018-02-21 18:54:20 -0500

    Add app demo link

--- client/components/demo.js
+++
@@ -1,8 +0,0 @@
-angular.module('app')
-  .controller('demoCtrl', function () {
-    console.log('Inside demo controller!');
-  })
-  .component('demo', {
-    controller: 'demoCtrl',
-    templateUrl: 'templates/demo.html'
-  });
